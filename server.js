c7a2e360a0a962de2c3b0f042194867d1af4a6a8
Rafal Pysz <kuchasz1@o2.pl>
2018-08-11 10:32:42 +0200

    another change

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.enable('trust proxy');
 const ips = [];
 
 app.get('/ping', (req, res)=>{
-    ips.push(req.ip);
+    ips.push(req.ip.split(":")[0]);
     res.send(req.ip);
 });
 
 app.get("/", (req, res)=>{
-    const uniqueIps = ips.map(ip => ip.split(":")[0]).filter(function(item, pos) {
+    const uniqueIps = ips.filter(function(item, pos) {
         return ips.indexOf(item) == pos;
     });
 
